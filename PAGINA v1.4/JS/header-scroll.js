c66ad195f903adf2a3ac982e1951b44369180a26
joartms <joaquinretamosa13@gmail.com>
2026-06-08 21:12:19 -0300

    Add files via upload

--- PAGINA v1.4/JS/header-scroll.js
+++ PAGINA v1.4/JS/header-scroll.js
@@ -1,8 +1,5 @@
 const header = document.querySelector('.header');
-
-// Escuchamos el evento de scroll en la ventana
 window.addEventListener('scroll', () => {
-  // Verificamos si el scroll vertical es mayor a 50 píxeles
   if (window.scrollY > 50) {
     header.classList.add('scrolled');
   } else {
--- PAGINA v1.4/JS/popup.js
+++ PAGINA v1.4/JS/popup.js
@@ -1,14 +1,9 @@
-// Definición de variables
 const botonAbrir = document.getElementById("inicio-sesion-popup");
 const botonCerrar = document.getElementById("cerrar-popup-boton");
 const popUp = document.getElementById("popup");
-
-// Funcionamiento del botón para abrir/mostrar el pop-up
 botonAbrir.addEventListener("click", () => {
     popUp.showModal();
 });
-
-// Funcionamiento del botón para cerrar el pop-up
 botonCerrar.addEventListener("click", () => {
     popUp.close();
 });
